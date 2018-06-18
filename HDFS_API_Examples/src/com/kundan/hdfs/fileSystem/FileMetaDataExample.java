99ca7b9f380512f1dcecd1399ce082def924667c
tr-dt-043 <tr-dt-043@TR-DT-043>
2018-06-18 18:50:09 +0530

    File Metatdata and List Files

--- HDFS_API_Examples/src/com/kundan/hdfs/fileSystem/FileMetaDataExample.java
+++ HDFS_API_Examples/src/com/kundan/hdfs/fileSystem/FileMetaDataExample.java
@@ -7,8 +7,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.RemoteIterator;
-import org.apache.hadoop.fs.permission.FsPermission;
 
 public class FileMetaDataExample {
 	
